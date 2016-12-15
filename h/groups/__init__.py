d012b0facebfd10bdf0ed07aa193f179872df7ab
chdorner <christof@chdorner.com>
2016-12-15 19:50:15 +0100

    Configure group search filter with memex
    This way we always make sure that memex only returns search results that
    the user is allowed to see.

--- h/groups/__init__.py
+++ h/groups/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from h.groups import util
+from h.groups import search
 
 
 def includeme(config):
     config.register_service_factory('.services.groups_factory', name='groups')
     config.memex_set_groupfinder('h.groups.util.fetch_group')
+    config.memex_add_search_filter(search.AuthFilter)
